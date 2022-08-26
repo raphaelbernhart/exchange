ae6354cf994bda788ebc2eee8766cf69fc376270
raphaelbernhart <mail@raphaelbernhart.at>
2022-08-26 17:00:01 +0200

    🎨 Format code

--- src/routes/uploadView.ts
+++ src/routes/uploadView.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express"
-import path from 'path'
+import { Request, Response } from "express";
+import path from "path";
 
-export default async (req: Request|any, res: Response) => {
+export default async (req: Request | any, res: Response) => {
     const apiUrl: string = process.env.API_URL;
-    res.render(path.join(__dirname+"/../views/upload.ejs"), {
+    res.render(path.join(__dirname + "/../views/upload.ejs"), {
         apiUrl: apiUrl,
-        maxFileSize: process.env.MAX_FILE_SIZE
+        maxFileSize: process.env.MAX_FILE_SIZE,
     });
-}
+};
